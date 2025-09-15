f7872bd7b638ea49adbbc5591b3d6c3f711b7b2b
jyotigupta23 <jyotigupta141place@gmail.com>
2025-09-15 15:21:28 +0530

    changes checking

--- src/Arrays/_03CheckIfTheArrayIsSorted.java
+++ src/Arrays/_03CheckIfTheArrayIsSorted.java
@@ -2,6 +2,6 @@ package Arrays;
 
 public class _03CheckIfTheArrayIsSorted {
     static void main(String[] args) {
-    System.out.println("just checking commit");
+    System.out.println("just checking commit....");
     }
 }
